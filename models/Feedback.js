d947310fc48210ea92c71c9d7227f4a9e059eed1
venkata sravan pulluru <venkatasravan_pulluru@srmap.edu.in>
2025-08-16 13:24:04 +0530

    Update Feedback.js

--- models/Feedback.js
+++ models/Feedback.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
-const FeedbackSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  message: { type: String, required: true }
+import mongoose from "mongoose";
+
+const feedbackSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String },
+  message: { type: String, required: true },
+  rating: { type: Number, min: 1, max: 5 },
 }, { timestamps: true });
-module.exports = mongoose.model('Feedback', FeedbackSchema);
+
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+export default Feedback;
